ff6aca940d851956e815df546d5fbeec82bf4d41
pxwang1 <xihong@gmail.com>
2015-02-28 21:24:02 -0800

    changed a lot since Jan 15

--- Character.java
+++ Character.java
@@ -8,6 +8,18 @@ import java.util.List.*;
  */
 public class Character extends Actor
 {
+   int level;
+  
+  
+   
+
+    public Character()
+    {
+       checkWorld();
+       level=1;
+       
+       
+    }
 
     /**
      * Act - do whatever the Character wants to do. This method is called whenever
@@ -23,57 +35,119 @@ public class Character extends Actor
         checkLever();
         checkTrap();
         checkEndgate();
+        
 
     }
-    
+
     public void checkEndgate()
     {
         if(canSeeLeft(Endgate.class)&& !checkLockStatus()||canSeeRight(Endgate.class)&& !checkLockStatus()||canSeeUp(Endgate.class)&& !checkLockStatus()||canSeeDown(Endgate.class)&& !checkLockStatus())
         {
             Greenfoot.playSound("explosion.wav");
             //display win
-            WorldTutorial.gameStatus = 2 ;
+            //WorldTutorial.gameStatus =3 ;
+            level++;
+            if(level==2)
+            {
+                getWorld().removeObject(this);
+                Greenfoot.setWorld(new WORLDTWO());
+                
+
+                WORLDTWO.lockStatus=1;
+            }
+            if(level==3)
+            {
+                getWorld().removeObject(this);
+                Greenfoot.setWorld(new WORLDTHREE());
+                level++;
+
+                //WORLDTWO.gameStatus=2;
+            }
+
         }
     }
-    
+
     public void checkTrap()
     {
         if(canSeeLeft(Trap.class)||canSeeRight(Trap.class)||canSeeUp(Trap.class)||canSeeDown(Trap.class))
         {
             Greenfoot.playSound("explosion.wav");
             //display lose
-            WorldTutorial.gameStatus = 2 ;
-            
-        }
-    }
-    
-    public void checkLever()
-    {
-        if(canSeeLeft(Lever.class)&& checkLockStatus()||canSeeRight(Lever.class)&& checkLockStatus()||canSeeUp(Lever.class)&& checkLockStatus()||canSeeDown(Lever.class)&& checkLockStatus())
-        {
-            WorldTutorial.lockStatus =2;
-            Greenfoot.playSound("buzz.wav");
+
+            WORLDTWO.gameStatus=2;
+            WorldTutorial.gameStatus=2;
+
         }
     }
 
-    public  boolean checkLockStatus()
+    private void checkLever()
     {
-        if(WorldTutorial.lockStatus==1)
+        if(canSeeLeft(Lever.class)&& checkLockStatus()||canSeeRight(Lever.class)&& checkLockStatus()||canSeeUp(Lever.class)&& checkLockStatus()||canSeeDown(Lever.class)&& checkLockStatus())
         {
-            return true;
+
+            WorldTutorial.lockStatus =2;
+
+            WORLDTWO.lockStatus=2;
+
+            Greenfoot.playSound("buzz.wav");
+        }
+    }
+    
+    public void checkWorld()
+    {
+        if (getWorld() instanceof WorldTutorial)
+        {
+            level=1;
+        }
+        
+        if (getWorld() instanceof WORLDTWO)
+        {
+            level=2;
+        }
+        
+    }
+
+    private  boolean checkLockStatus()
+    {
+        if (level==1)
+        {
+            if(WorldTutorial.lockStatus==1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        if(level==2)
+        {
+            if(WORLDTWO.lockStatus==1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
         else
         {
+
             return false;
         }
     }
 
     public void checkZooKeeper()
     {
-        if(canSeeLeft(Zookeeper.class)||canSeeRight(Zookeeper.class)||canSeeUp(Zookeeper.class)||canSeeDown(Zookeeper.class))
+        if(canSeeLeft(Zookeeper.class)||canSeeRight(Zookeeper.class)||canSeeUp(Zookeeper.class)||canSeeDown(Zookeeper.class)||canSeeLeft(Zookeeper2.class)||canSeeRight(Zookeeper2.class)||canSeeUp(Zookeeper2.class)||canSeeDown(Zookeeper2.class)||canSeeLeft(Zookeeper3.class)||canSeeRight(Zookeeper3.class)||canSeeUp(Zookeeper3.class)||canSeeDown(Zookeeper3.class))
         {
-            
-            WorldTutorial.gameStatus = 2 ;
+            //do this for all the other worlds too
+
+            WORLDTWO.gameStatus=2;
+            WorldTutorial.gameStatus = 2;
         }
     }
 
@@ -161,10 +235,6 @@ public class Character extends Actor
         return actor != null; 
     }
 
-    
-    
-    
-    
     public boolean canSeeLeft2(Class clss) 
     {
         Actor actor = getOneObjectAtOffset(-23, 0, clss); 
@@ -212,5 +282,6 @@ public class Character extends Actor
         Actor actor = getOneObjectAtOffset(0, 24, clss); 
         return actor != null; 
     }
-    
+
 }
+
